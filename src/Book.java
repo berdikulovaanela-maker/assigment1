0af9a84be237565c0b642e25205efbff5cac1aa3
Berdikulova Anelya <berdikulovaanela@gmail.com>
2025-12-19 17:52:28 +0500

    .

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
-
 public class Book {
+    private int id;
+    private static int idGen=1;
+    private String title;
+    private String author;
+    private int year;
+    private boolean available;
+    public Book() {
+        id = idGen++;
+        available = true;
+    }
+    public Book(String title, String author, int year) {
+        this();
+        setTitle(title);
+        setAuthor(author);
+        setYear(year);
+    }
+    public void setTitle(String title) {
+        if(title==null) throw new IllegalArgumentException();
+        this.title = title;
+    }
+    public void setAuthor(String author) {
+        if(author==null) throw new IllegalArgumentException();
+        this.author = author;
+    }
+    public void setYear(int year) {
+        if(year > 2025 || year < 1500) throw new IllegalArgumentException();
+        this.year = year;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void markAsBorrowed() {
+        available = false;
+    }
+    public void markAsReturned() {
+        available = true;
+    }
+    public boolean isAvailable() {
+        return available;
+    }
+    @Override
+    public String toString() {
+        return " Book [" + id + "] [title = " + title + ", author = " + author + ", year = " + year + "]";
+    }
 }
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
-public class rectangle {
+public class Rectangle {
+    private double width;
+    private double height;
+    private int id;
+    private static int idGen = 1;
+    public Rectangle() {
+        this.width = 1.0;
+        this.height = 1.0;
+        this.id = idGen++;
+    }
+    public Rectangle(double width, double height) {
+        this();
+        setWidth(width);
+        setHeight(height);
+    }
+    public double GetWidth() {
+        return width;
+    }
+    public void setWidth(double width) {
+        if(width <= 0) throw new IllegalArgumentException();
+        this.width = width;
+    }
+    public double GetHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        if(height <= 0) throw new  IllegalArgumentException();
+        this.height = height;
+    }
+    public double area(){
+        return this.width * this.height;
+    }
+    public double perimeter(){
+        return (this.width + this.height)*2;
+    }
+    public String toString(){
+        return "Rectangle [width=" + width + ", height=" + height + ", id=" + id + "]";
+    }
 }
